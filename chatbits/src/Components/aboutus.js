97559f6a45aeda4a6a82a59362cd24516d72df8f
LLOYD <laccollege19@gmail.com>
2023-02-16 16:01:38 +0530

    final

--- chatbits/src/Components/aboutus.js
+++ chatbits/src/Components/aboutus.js
@@ -1,23 +1,52 @@
+import './Css/AboutUsStyle.css'
+import './Css/square.css'
+// import './AbUS.js'
+
 function contact() {
   return (
     <>
       <div>
-        <div className="container d-flex justify-content-center">
-          <div className="team">
-          <h4><b>About us</b></h4>
-          <h4>What motivated us to make This</h4>
+      <div class="aboutUsConainer">
+                <section class="section1">
+                   
+         
+                   <div id="landingPageAboutUsTitle"> 
+                    <div class="AboutRED" id="aboutUstext1">Hello </div> 
+                    <div class="AboutYELLOW"  id="aboutUstext2">from</div>
+                    <div class="AboutBLUE titleCRAFT"  id="aboutUstext3">CHATHOUSE</div>
 
-          <p>Grocery is on of the most important basic needs for people.The grocery app would allow us to buy grocery from the comfort of their home and thus ,saving us a greate amount of time.Also people will not have to worry about the quality of production as we are going to segregate the best of them.</p>
-          <b>Features</b>
-          <ul>
-            <li>Buying and selling of grocery</li>
-            <li>Quick recipe</li>
-            <li>Recommendation algorithm(ML) for better experice</li>
-          </ul>
-          </div>
-         
-         
-        </div>
+                    <div class="titleCRAFT"  id="WhoWeAre"><p>We are committed to providing our customers with a memorable dining experience. Our friendly staff, cozy atmosphere, and exceptional food make ChaatHouse the perfect spot for a casual meal with friends, a quick lunch break, or even a takeout dinner.</p></div>
+                </div>
+                
+
+                <div class="your-element" data-tilt>
+                <div class="CARDabout">
+                        <div id="CARDaboutTitle"> Our beginning...</div>
+                        <p>Welcome to ChaatHouse, the newest addition to the vibrant food scene. We are a Chaat-only restaurant that prides ourselves on serving authentic, delicious, and affordable Indian street food.
+                            Our story began in 2020 when we realized that there was a lack of quality Chaat options in the area. We decided to fill this gap by creating a restaurant that specializes in Chaat dishes, made from fresh and locally sourced ingredients.
+                            </p>
+                </div>
+               </div>
+
+               
+                <div class="SquareContainer">
+                    <div class="Square"></div>
+                    <div class="Square"></div>
+                    <div class="Square"></div>
+                    <div class="Square"></div>
+                    <div class="Square"></div>
+                    <div class="Square"></div>
+                    <div class="Square"></div>
+                    <div class="Square"></div>
+                    <div class="Square"></div>
+                    <div class="Square"></div>
+                    <div class="Square"></div>
+                </div>
+
+
+                </section>
+                
+            </div>
       </div>
     </>
   );
